6a00f4357bf0ff787b340326f42b351adb249619
varunGupta1234567890 <gvarun4220@gmail.com>
2025-07-10 18:47:34 +0530

    add models for comments, likes, playlists, and tweets with pagination support

---
+++ src/models/playlist.model.js
@@ -0,0 +1,26 @@
+import mongoose, {Schema} from "mongoose";
+
+const playlistSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    videos: [  //video kafi hoti h isliye array bnao
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+}, {timestamps: true})
+
+
+
+export const Playlist = mongoose.model("Playlist", playlistSchema)
